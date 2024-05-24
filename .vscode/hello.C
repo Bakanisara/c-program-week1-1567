5cd55d5fd29547abb50ec47842e02f34e996ebed
SUREE7913 <suree7913@gmail.com>
2024-05-24 16:24:03 +0700

    first commit

---
+++ .vscode/hello.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // Create integer vartable
+    int age = 16;
+    // Print hello to the screen
+    printf("Hello World\n");
+    printf("my name is Mr.John sara\n");
+    printf("Age: %d\n", age);
+    return 0;
+}
